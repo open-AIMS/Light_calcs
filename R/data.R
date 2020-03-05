a40cc166ce740a2efdf21d1e4c9f6e12b507bd67
beckyfisher <rebeccafisher76@gmail.com>
2020-03-05 13:32:16 +0800

    added data documentation

---
+++ R/data.R
@@ -0,0 +1,40 @@
+
+
+
+#' @title absorption.dat
+#' @description Absorption data
+#' @format A data frame with 302 obs. of  2 variables:
+#' \describe{
+#'   \item{\code{nm}}{double Wavelength in nm}
+#'   \item{\code{Absorption}}{double Normalised absorption spectra}
+#'}
+#' @source \url{}
+"absorption.dat"
+
+
+#' @title model.dat
+#' @description Model data derived from IMO observations in Cleveland Bay
+#' @format A data frame with 351 obs. of  7 variables:
+#' \describe{
+#'   \item{\code{nm}}{integer Wavelength in nm}
+#'   \item{\code{Kd_M}}{Kd_M}
+#'   \item{\code{Kd_C}}{Kd_C}
+#'   \item{\code{Es_C0}}{double Es_C0}
+#'   \item{\code{Es_C1}}{double Es_C1}
+#'   \item{\code{Es_C2}}{Es_C2}
+#'   \item{\code{Es_C3}
+#'}
+#' @source \url{}
+"model.dat"
+
+
+#' @title solar.zenith.dat
+#' @description MSolar zenith data
+#' @format A data frame with 91 obs. of  2 variables:
+#' \describe{
+#'   \item{\code{sza}}{integer Solar zenith angle}
+#'   \item{\code{frac}}{frac}
+#'}
+#' @source \url{}
+"solar.zenith.dat"
+
