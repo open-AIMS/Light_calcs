b949eb6a9c9966740b27e4c4e76b897946c3de28
Diego Barneche <diego.barneche@ymail.com>
2020-03-06 14:02:13 +0800

    one more

--- R/IMO_Light_model.R
+++ R/IMO_Light_model.R
@@ -53,14 +53,14 @@ calc.PAR=function(SZA=0,
   # read in supporting data
 
    if(missing(model.dat)){
-    model.dat=Light.calcs::modelDat
+    model.dat=Light_calcs::modelDat
    }
    if(missing(absorption.dat)){
-    absorption.dat=Light.calcs::absorptionDat
+    absorption.dat=Light_calcs::absorptionDat
    }
    if(missing(solar.zenith.dat)){
      #browser()
-     solar.zenith.dat=Light.calcs::solarZenithDat
+     solar.zenith.dat=Light_calcs::solarZenithDat
    }
 
   # extract variables
