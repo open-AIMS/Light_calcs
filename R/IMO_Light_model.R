9b4a3ad08a0d91a94f9cbb1991ec50b6c5b4e6cf
beckyfisher <rebeccafisher76@gmail.com>
2020-03-06 09:03:48 +0800

    troubleshooting

--- R/IMO_Light_model.R
+++ R/IMO_Light_model.R
@@ -39,9 +39,9 @@
 calc.PAR=function(SZA=0,
                   TSS=0,
                   Depth=0,
-                  solar.zenith.dat=Light.calcs::solarZenithDat,
-                  model.dat=Light.calcs::modelDat,
-                  absorption.dat=Light.calcs::absorptionDat,
+                  solar.zenith.dat=NA,
+                  model.dat=NA,
+                  absorption.dat=NA,
                   ND.adj=1,
                   nm.range=c(400,700)){
   #SZA=0;TSS=0.5;Depth=5;solar.zenithDat=NA;modelDat=NA;absorptionDat=NA;nm.range=c(400,700)
@@ -52,15 +52,15 @@ calc.PAR=function(SZA=0,
 
   # read in supporting data
 
-  # if(length(model.dat)==1){
-  #  model.dat=Light.calcs::modelDat
-  # }
-  # if(length(absorption.dat)==1){
-  #  absorption.dat=Light.calcs::absorptionDat
-  # }
-  # if(length(solar.zenith.dat)==1){
-  #   solar.zenith.dat=Light.calcs::solarZenithDat
-  # }
+   if(length(model.dat)==1){
+    model.dat=modelDat
+   }
+   if(length(absorption.dat)==1){
+    absorption.dat=absorptionDat
+   }
+   if(length(solar.zenith.dat)==1){
+     solar.zenith.dat=solarZenithDat
+   }
 
   # extract variables
   Solar.Zenith.Angle=SZA
